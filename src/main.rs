9ece36f9dd10b8501a74dbfb5219cb7a3bbfe83e
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-25 19:29:36 +0900

    feat: solve bottom 3 rows and pretty print boards

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
+use std::iter;
+
+use itertools::{EitherOrBoth, Itertools};
+
 mod enumerate_2d;
 mod puzzle;
 
+fn concat_linewise(a: &str, b: &str) -> String {
+    a.lines()
+        .zip_longest(b.lines())
+        .map(|x| match x {
+            EitherOrBoth::Both(a, b) => a.to_owned() + b,
+            EitherOrBoth::Left(a) => a.to_owned(),
+            EitherOrBoth::Right(b) => b.to_owned(),
+        })
+        .join("\n")
+}
+
 fn main() {
-    let mut problem: puzzle::Board = "uldlrrulrldlrulu".parse().unwrap();
+    let problem: puzzle::Board = "uldlrrulrldlrulu".parse().unwrap();
 
-    let solution = puzzle::solve_bottom_3_rows(problem.clone());
+    let solver = puzzle::solve_nth_row(3)
+        .and_then(|| puzzle::solve_nth_row(2))
+        .and_then(|| puzzle::solve_nth_row(1));
+    let puzzle::Solution(pokes) = solver.run(&problem);
 
-    println!("{:?}", solution);
-    println!("{}", problem);
+    let intermediate_boards = pokes.iter().scan(problem.clone(), |b, p| {
+        *b = b.poke(p);
+        Some(b.clone())
+    });
 
-    for m in solution.moves {
-        problem = problem.poke(m.x, m.y);
-        println!("{}", problem);
-    }
+    let s = iter::once(problem)
+        .chain(intermediate_boards)
+        .zip_longest(pokes.iter())
+        .map(|x| match x {
+            EitherOrBoth::Both(b, p) => b.to_string_with_highlight(p),
+            EitherOrBoth::Left(b) => b.to_string(),
+            EitherOrBoth::Right(_) => unreachable!(),
+        })
+        .chunks(4)
+        .into_iter()
+        .map(|chunk| chunk.fold("".to_owned(), |acc, s| concat_linewise(&acc, &s)))
+        .join("\n");
+    println!("{}", s);
 }
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,10 +1,10 @@
-use std::{fmt::Display, ops::Rem, str::FromStr};
+use std::{fmt::Display, iter, str::FromStr};
 
 use itertools::Itertools;
 
 use crate::enumerate_2d::Enumerate2dTrait;
 
-#[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 enum Arrow {
     Up,
     Right,
@@ -45,7 +45,7 @@ impl Arrow {
     }
 
     /// How many CW rotations are needed to go from `self` to `other`.
-    fn distance_cw(&self, other: &Arrow) -> u8 {
+    fn cw_distance_to(&self, other: &Arrow) -> u8 {
         match self {
             Arrow::Up => match other {
                 Arrow::Up => 0,
@@ -102,7 +102,7 @@ impl Display for Board {
         writeln!(f, "┌────────────┐")?;
         for row in self.arrows.chunks(4) {
             match &row {
-                &[a, b, c, d] => writeln!(f, "│ {}  {}  {}  {} │", a, b, c, d)?,
+                [a, b, c, d] => writeln!(f, "│ {}  {}  {}  {} │", a, b, c, d)?,
                 _ => unreachable!(),
             }
         }
@@ -111,108 +111,213 @@ impl Display for Board {
 }
 
 impl Board {
+    /// Stringifies the board with highlight at `p`.
+    pub(crate) fn to_string_with_highlight(&self, p: &Poke) -> String {
+        let px = p.x.into();
+        let py = p.y.into();
+
+        let mut buf = String::new();
+        buf += "┌────────────┐\n";
+        buf += &self
+            .arrows
+            .iter()
+            .enumerate_2d(4)
+            .map(|(_, (ax, ay), arrow)| {
+                if ax == px && ay == py {
+                    format!("\x1b[7m {} \x1b[0m", arrow)
+                } else {
+                    format!(" {} ", arrow)
+                }
+            })
+            .chunks(4)
+            .into_iter()
+            .map(|it| match &it.collect::<Vec<_>>()[..] {
+                [a, b, c, d] => format!("│{}{}{}{}│", a, b, c, d),
+                _ => unreachable!(),
+            })
+            .join("\n");
+        buf += "\n└────────────┘";
+        buf
+    }
+
     fn are_bottom_n_rows_aligned(&self, n: u8) -> bool {
         let n = n as usize;
         self.arrows.iter().skip(n * 4).all_equal()
     }
 
-    pub(crate) fn poke(&self, x: u8, y: u8) -> Self {
+    fn flip_h(&self) -> Board {
+        Board {
+            arrows: self
+                .arrows
+                .chunks(4)
+                .flat_map(|row| {
+                    let mut row = row.to_vec();
+                    row.reverse();
+                    row
+                })
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
+        }
+    }
+
+    pub(crate) fn poke(&self, p: &Poke) -> Board {
+        let px: i16 = p.x.into();
+        let py: i16 = p.y.into();
         let arrows = self
             .arrows
             .iter()
             .enumerate_2d(4)
-            .map(|((ax, ay), arrow)| {
-                let dx = x as i64 - ax as i64;
-                let dy = y as i64 - ay as i64;
-                match (dx, dy) {
+            .map(|(_, (ax, ay), &arrow)| {
+                let ax: i16 = ax.try_into().unwrap();
+                let ay: i16 = ay.try_into().unwrap();
+                match (px - ax, py - ay) {
                     (-1..=1, -1..=1) => arrow.rotate_cw(),
-                    _ => arrow.clone(),
+                    _ => arrow,
                 }
             })
             .collect::<Vec<_>>()
             .try_into()
-            .expect("want exactly 16 elements");
-        Self { arrows }
+            .unwrap();
+        Board { arrows }
+    }
+
+    pub(crate) fn poke_many(&self, ps: &[Poke]) -> Board {
+        ps.iter().fold(self.clone(), |b, p| b.poke(p))
     }
 }
 
-#[derive(Debug)]
-pub(crate) struct Move {
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct Poke {
     pub(crate) x: u8,
     pub(crate) y: u8,
 }
 
+impl Poke {
+    fn flip_h(&self) -> Poke {
+        Poke {
+            x: 4 - self.x,
+            y: self.y,
+        }
+    }
+}
+
+pub(crate) struct Solution(pub(crate) Vec<Poke>);
+
 #[derive(Debug)]
-pub(crate) struct Solution {
-    pub(crate) moves: Vec<Move>,
-    pub(crate) result: Board,
-}
+pub(crate) struct Solver<Run>(Run)
+where
+    Run: FnOnce(&Board) -> Solution;
 
-impl Solution {
-    fn and_then<F>(self, f: F) -> Self
+impl<RunA> Solver<RunA>
+where
+    RunA: FnOnce(&Board) -> Solution,
+{
+    pub(crate) fn and_then<F, RunB>(self, f: F) -> Solver<impl FnOnce(&Board) -> Solution>
     where
-        F: FnOnce(Board) -> Self,
+        F: FnOnce() -> Solver<RunB>,
+        RunB: FnOnce(&Board) -> Solution,
     {
-        let mut s = f(self.result);
-        let mut moves = self.moves;
-        moves.append(&mut s.moves);
-        Solution {
-            moves,
-            result: s.result,
-        }
+        Solver(|board| {
+            let Solution(mut pokes1) = self.run(board);
+            let Solution(mut pokes2) = f().run(&board.poke_many(&pokes1));
+            pokes1.append(&mut pokes2);
+            Solution(pokes1)
+        })
+    }
+
+    pub(crate) fn run(self, b: &Board) -> Solution {
+        self.0(b)
     }
 }
 
-/// Solves the `n`th row of `b`.
-/// The bottom `n - 1` rows must already be solved.
+/// Solves the `n`th row.
+/// Bottom `n - 1` rows must already be solved.
 /// `n` must be 1, 2, or 3.
-fn solve_nth_row(b: Board, n: u8) -> Solution {
-    let mut boards = vec![b];
-    let mut history = vec![];
+pub(crate) fn solve_nth_row(n: u8) -> Solver<impl FnOnce(&Board) -> Solution> {
+    Solver(move |b| {
+        let mut boards = vec![b.clone()];
+        let mut poke_count = 0;
 
-    loop {
-        if let Some((i, result)) = boards
-            .iter()
-            .enumerate()
-            .find(|(_, b)| b.are_bottom_n_rows_aligned(n))
-        {
-            let moves = (0..history.len())
-                .map(|j| {
-                    let x = i / 4usize.pow(j as u32);
-                    let x = (x % 4) as u8;
-                    Move { x, y: n - 1 }
-                })
+        loop {
+            if let Some((i, _)) = boards
+                .iter()
+                .enumerate()
+                .find(|(_, b)| b.are_bottom_n_rows_aligned(n))
+            {
+                let pokes = (0..poke_count)
+                    .map(|nth_poke| {
+                        let x = i / 4usize.pow(nth_poke);
+                        let x = (x % 4).try_into().unwrap();
+                        Poke { x, y: n - 1 }
+                    })
+                    .collect();
+                return Solution(pokes);
+            }
+
+            boards = boards
+                .iter()
+                .flat_map(|b| (0..4).map(|x| b.poke(&Poke { x, y: n - 1 })))
                 .collect();
-            return Solution {
-                moves,
-                result: result.clone(),
-            };
+            poke_count += 1;
         }
-
-        let new_boards: Vec<_> = boards
-            .iter()
-            .flat_map(|b| (0..4).map(|x| b.poke(x, n - 1)))
-            .collect();
-        history.push(boards);
-        boards = new_boards;
-    }
+    })
 }
 
-pub(crate) fn solve_bottom_3_rows(b: Board) -> Solution {
-    solve_nth_row(b, 3)
-        .and_then(|b| solve_nth_row(b, 2))
-        .and_then(|b| solve_nth_row(b, 1))
-}
-
-/// Solves the first row of `b`.
-/// The bottom 3 rows must already be solved.
-fn solve_first_row(board: Board) -> Solution {
-    let (a, b, c, d) = match &board.arrows[0..4] {
-        [a, b, c, d] => (a, b, c, d),
-        _ => unreachable!(),
-    };
-
-    if a == b && b == c && c == d {}
-
-    todo!()
-}
+// pub(crate) fn solve_bottom_3_rows(b: Board) -> Solver {
+//     solve_nth_row(b, 3)
+//         .and_then(|b| solve_nth_row(b, 2))
+//         .and_then(|b| solve_nth_row(b, 1))
+// }
+//
+// /// Solves the first row of `b`.
+// /// The bottom 3 rows must already be solved.
+// fn solve_first_row(board: Board) -> Solver {
+//     // ┌────────────┐
+//     // │ a  b  c  d │
+//     // │ e  _  _  h │
+//     // │ _  _  _  _ │
+//     // │ _  _  _  _ │
+//     // └────────────┘
+//
+//     fn xxxx(board: Board) -> Solver {
+//         let a = board.arrows[0];
+//         let e = board.arrows[4];
+//         let n = e.cw_distance_to(&a).into();
+//         Solver {
+//             moves: [Poke { x: 0, y: 2 }, Poke { x: 3, y: 2 }].repeat(n),
+//             result: Board { arrows: [a; 16] },
+//         }
+//     }
+//
+//     fn xxx_(board: Board) -> Solver {
+//         let b = board.arrows[1];
+//         let d = board.arrows[3];
+//         let e = board.arrows[4];
+//         let h = board.arrows[7];
+//         let h_to_d = h.cw_distance_to(&d).into();
+//         let d_to_b = d.cw_distance_to(&b).into();
+//         let b_to_d = b.cw_distance_to(&d).into();
+//         let m1 = iter::repeat(Poke { x: 0, y: 2 }).take(h_to_d);
+//         let m2 = iter::repeat(Poke { x: 3, y: 2 }).take(h_to_d);
+//         let m3 = iter::repeat(Poke { x: 1, y: 2 }).take(d_to_b);
+//         let m4 = iter::repeat(Poke { x: 1, y: 0 }).take(b_to_d);
+//         let m5 = iter::repeat(Poke { x: 1, y: 3 }).take(b_to_d);
+//         // Move {
+//         //     moves: m1.chain(m2).chain(m3).chain(m4).chain(m5).collect(),
+//         // }
+//         todo!()
+//     }
+//
+//     let (a, b, c, d) = match &board.arrows[0..4] {
+//         [a, b, c, d] => (a, b, c, d),
+//         _ => unreachable!(),
+//     };
+//
+//     if a == b && b == c && c == d {
+//         xxxx(board)
+//     // } else if a {
+//     } else {
+//         todo!()
+//     }
+// }
