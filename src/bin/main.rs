2395a2ec98edfe45c658cb5c3ac7f840f43e2afd
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-29 01:30:30 +0900

    feat: it works

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,11 +2,14 @@ use std::{fs, thread::sleep, time::Duration};
 
 use anyhow::Result;
 use device_query::{DeviceQuery, DeviceState, Keycode};
+use enigo::{Enigo, MouseButton, MouseControllable};
+use mouse_rs::{types::keys::Keys, Mouse};
 use scrap::{Capturer, Display};
 use solve_arrow_puzzle::{
     config::Config,
-    gui::{ArrowToColor, Screenshot},
+    gui::{ArrowToColor, Point, Screenshot},
     puzzle::{Board, Row},
+    solve::pokes_to_align_board,
 };
 
 fn screenshot_to_board(
@@ -27,13 +30,32 @@ fn screenshot_to_board(
 }
 
 fn main() -> Result<()> {
+    // let mouse = Mouse::new();
+    // println!("start");
+    // sleep(Duration::from_millis(5000));
+    // println!("press");
+    // mouse.press(&Keys::LEFT).unwrap();
+    // sleep(Duration::from_millis(500));
+    // println!("release");
+    // mouse.release(&Keys::LEFT).unwrap();
+    // return Ok(());
+
+    // let mut enigo = Enigo::new();
+    // println!("start");
+    // sleep(Duration::from_millis(5000));
+    // println!("down");
+    // enigo.mouse_down(MouseButton::Left);
+    // sleep(Duration::from_millis(500));
+    // println!("up");
+    // enigo.mouse_up(MouseButton::Left);
+
     // TODO: `Dimensions` or something
     let config: Config = fs::read_to_string("config.json")?.parse()?;
     let arrow_to_color: ArrowToColor =
         fs::read_to_string("arrow_to_color.json")?.parse()?;
 
-    println!("press shift to begin");
     let state = DeviceState::new();
+    println!("press shift to begin");
     loop {
         sleep(Duration::from_millis(10));
         let keys = state.get_keys();
@@ -42,15 +64,44 @@ fn main() -> Result<()> {
             .any(|k| matches!(k, Keycode::LShift | Keycode::RShift))
         {
             break;
+        } else if keys.contains(&Keycode::Backspace) {
+            return Ok(());
         }
     }
 
     let mut capturer = Capturer::new(Display::primary()?)?;
 
-    let screenshot = Screenshot::take(&mut capturer)?;
-    let board = screenshot_to_board(&screenshot, &config, &arrow_to_color);
+    while !state.get_keys().contains(&Keycode::Backspace) {
+        sleep(Duration::from_millis(150));
 
-    println!("{}", board);
+        let screenshot = Screenshot::take(&mut capturer)?;
+        let board = screenshot_to_board(&screenshot, &config, &arrow_to_color);
+        let pokes = pokes_to_align_board(&board);
+        println!("{} pokes", pokes.len());
+
+        let mut enigo = Enigo::new();
+        for p in pokes {
+            let Point { x, y } = config.arrow_position(&p);
+            click(&mut enigo, x as _, y as _, Duration::from_millis(1));
+        }
+        sleep(Duration::from_millis(150));
+        click(
+            &mut enigo,
+            config.claim_button_position.x as _,
+            config.claim_button_position.y as _,
+            Duration::from_millis(1),
+        );
+    }
 
     Ok(())
+
+    // Ok(())
+}
+
+fn click(enigo: &mut Enigo, x: i32, y: i32, delay: Duration) {
+    enigo.mouse_move_to(x, y);
+    sleep(delay);
+    enigo.mouse_down(MouseButton::Left);
+    sleep(delay);
+    enigo.mouse_up(MouseButton::Left);
 }
--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use serde::Deserialize;
 
-use crate::gui::Point;
+use crate::{gui::Point, puzzle::BoardPoke};
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -16,5 +16,18 @@ impl FromStr for Config {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         serde_json::from_str(s)
+    }
+}
+
+impl Config {
+    pub fn arrow_position(&self, &BoardPoke(x, y): &BoardPoke) -> Point {
+        let x: u8 = x.into();
+        let y: u8 = y.into();
+        let x: i64 = x.into();
+        let y: i64 = y.into();
+        Point {
+            x: self.first_arrow_position.x + self.arrow_diameter * x,
+            y: self.first_arrow_position.y + self.arrow_diameter * y,
+        }
     }
 }
