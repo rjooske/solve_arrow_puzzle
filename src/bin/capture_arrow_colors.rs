fcd0478ca28e90e775b70878298f08902694ed08
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-31 22:48:58 +0900

    fix: update capture_arrow_colors to use the new library code

--- src/bin/capture_arrow_colors.rs
+++ src/bin/capture_arrow_colors.rs
@@ -2,28 +2,12 @@ use anyhow::{anyhow, Context, Result};
 use itertools::Itertools;
 
 use solve_arrow_puzzle::{
-    config::Config,
-    gui::{ArrowToColor, Color},
-    puzzle::Arrow,
+    gui::{ArrowToColor, Color, Dimensions, Point, Screenshot},
+    puzzle::{Arrow, BoardPoke, RowPoke},
 };
 use std::{collections::HashMap, env, fs, io, thread::sleep, time::Duration};
 
 use scrap::{Capturer, Display};
-
-fn capture_frame(c: &mut Capturer) -> io::Result<Vec<u8>> {
-    loop {
-        match c.frame() {
-            Ok(f) => return Ok(f.to_vec()),
-            Err(err) => {
-                if err.kind() == io::ErrorKind::WouldBlock {
-                    sleep(Duration::from_millis(1));
-                } else {
-                    return Err(err);
-                }
-            }
-        }
-    }
-}
 
 fn parse_arg(arg: &str) -> Result<Vec<Arrow>> {
     arg.chars()
@@ -70,43 +54,26 @@ fn main() -> Result<()> {
         env::args().collect::<Vec<_>>().try_into().unwrap();
     let arrows = parse_arg(&arg)?;
 
-    let config = fs::read_to_string("config.json")?;
-    let config: Config = serde_json::from_str(&config)?;
+    let dimensions: Dimensions =
+        fs::read_to_string("dimensions.json")?.parse()?;
 
-    let display = Display::primary()?;
-    let mut capturer = Capturer::new(display)?;
-    let width = capturer.width();
-    let height = capturer.height();
+    let mut capturer = Capturer::new(Display::primary()?)?;
+    let screenshot = Screenshot::take(&mut capturer)?;
 
-    let frame = capture_frame(&mut capturer)?;
-    let frame: Vec<_> = frame
-        .chunks(4)
-        .take(width * height)
-        .map(|x| match x {
-            &[b, g, r, _] => Color { r, g, b },
-            _ => unreachable!(),
-        })
-        .collect();
-
-    let colors = (0..4)
-        .cartesian_product(0..4)
+    let pokes = [RowPoke::A, RowPoke::B, RowPoke::C, RowPoke::D];
+    let colors = pokes
+        .into_iter()
+        .cartesian_product(pokes.into_iter())
         .map(|(y, x)| {
-            let x = config.first_arrow_position.x + config.arrow_diameter * x;
-            let y = config.first_arrow_position.y + config.arrow_diameter * y;
-            let i: usize = x as usize + width * y as usize;
-            match frame.get(i) {
-                Some(c) => Ok(*c),
-                None => Err(anyhow!(
-                    "arrow position ({}, {}) landed outside the screen",
-                    x,
-                    y
-                )),
-            }
+            let Point { x, y } = dimensions.arrow_position(&BoardPoke(x, y));
+            screenshot.at(x as _, y as _).with_context(|| {
+                format!("({}, {}) is outside the screen", x, y)
+            })
         })
         .collect::<Result<Vec<_>>>()?;
 
     let arrow_to_color = parse_arrow_to_color(&arrows, &colors)?;
-    println!("{}", serde_json::to_string(&arrow_to_color)?);
+    println!("{}", serde_json::to_string_pretty(&arrow_to_color)?);
 
     Ok(())
 }
